b605046d457452b54cbb2de9abd48cfeae56813b
John Foresight <foresight400@gmail.com>
2021-11-09 22:57:36 +0000

    rendering each URl, need to fix a bug

--- express_server.js
+++ express_server.js
@@ -9,13 +9,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-const templateVars = { urls: urlDatabase };
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// app.get("/urls/:shortURL", (req, res) => {
+//   const templateVars = { urls: urlDatabase };
+//   // res.render("urls_show", templateVars);
+//   req.params: {"urls_show", templateVars};
+// });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: /* What goes here? */ req.params.shortURL,
+  };
+  res.render("urls_show", templateVars);
+});
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
