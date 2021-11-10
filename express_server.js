4e51b353b2816e0d7ec4ac09e94ab26688e8cd94
John Foresight <foresight400@gmail.com>
2021-11-10 00:22:25 +0000

    installed a body parser, added a route to handle user input

--- express_server.js
+++ express_server.js
@@ -1,24 +1,37 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return `${Math.random().toString(36).slice(2)}`;
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// const bodyParser = require("body-parser");
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// app.get("/urls/:shortURL", (req, res) => {
-//   const templateVars = { urls: urlDatabase };
-//   // res.render("urls_show", templateVars);
-//   req.params: {"urls_show", templateVars};
-// });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
+  // res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
@@ -27,24 +40,7 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
