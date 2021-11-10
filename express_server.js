348774e43fec87c9638cd42759cfef4d00ef48ba
John Foresight <foresight400@gmail.com>
2021-11-10 21:45:04 +0000

    commneted out server based of incremental requiremets

--- express_server.js
+++ express_server.js
@@ -4,28 +4,35 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
 app.set("view engine", "ejs");
+
+// Generate a random string
 
 function generateRandomString() {
   return `${Math.random().toString(36).slice(2)}`;
 }
+
+// Reference database with our longURLs
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-// const bodyParser = require("body-parser");
+// urls endpoint/route to display our database
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// urls endpoint/route to display input form to update database with
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+// response message to display to client after update
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
@@ -33,18 +40,35 @@ app.post("/urls", (req, res) => {
   // res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+// route paramater to display requested data by key in database
+
+app.get("/urls/:id", (req, res) => {
+  // console.log(req.params); /** --> { shortURL: 'gddd' } */
+  const id = req.params.id;
+  let longUrl;
+  if (id) {
+    longUrl = urlDatabase[id];
+  } else {
+    res.send("no Id");
+  }
+
+  // if (!urlDatabase[shortURL]) {
+  //   res.send("sorry url does not exist");
+  //   return;
+  // }
   const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: /* What goes here? */ req.params.shortURL,
+    // shortURL: req.params.shortURL,
+    shortURL: id,
+    longURL: longUrl,
   };
+  // console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  // const longURL = ...
-  res.redirect(longURL);
-});
+// app.get("/u/:shortURL", (req, res) => {
+//   // const longURL = ...
+//   res.redirect(longURL);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
