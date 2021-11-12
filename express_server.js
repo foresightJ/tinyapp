5000a75071140b5a37d64ccf5e62844c6bcb0a48
John Foresight <foresight400@gmail.com>
2021-11-12 15:33:44 +0000

    slowly but surely, grit is the word

--- express_server.js
+++ express_server.js
@@ -7,59 +7,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // Generate a random string
-
 function generateRandomString() {
-  // console.log(Math.random().toString(36).slice(2, 8));
   return `${Math.random().toString(36).slice(2, 8)}`;
 }
 
 // Reference database with our longURLs
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 // urls endpoint/route to display our database
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// urls endpoint/route to display input form to update database with
-
+// client route request to create new entry in database
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// response message to display to client after update
+// server updates database with new entry from form and redirects to newly created URL
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body); // Log the POST request body to the console
-  const { longURL } = req.body;
+  const longURL = req.body.longURL;
+  // console.log(longURL);
   const shortURL = generateRandomString();
-  // res.render("urls_new");
-  // const {shortURL, longURL} = urlDatabase;
+
   urlDatabase[shortURL] = longURL;
-  // console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
-  // res.redirect("/urls/:shortURL");
+  // res.redirect("/urls");
 });
 
 // route paramater to display requested data by key in database
 
 app.get("/urls/:shortURL", (req, res) => {
-  // console.log(req);
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    /* What goes here? */
   };
-  console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
+// A redirect to the querried
 app.get("/u/:shortURL", (req, res) => {
   // const longURL = req.params.shortURL...
   const longURL = req.params.shortURL;
@@ -74,6 +65,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   // delete urlDatabase[req.params.shortURL];
   delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const shortURL = req.params.shortURL;
+  // console.log(req.body.editlongURL);
+  urlDatabase[shortURL] = req.body.editlongURL;
+
   res.redirect("/urls");
 });
 
